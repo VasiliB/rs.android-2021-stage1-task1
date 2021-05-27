8e1b2c12de8a37c96e2265f0773d087f6c9642d8
VasiliB <73487624+VasiliB@users.noreply.github.com>
2021-05-27 21:36:38 +0300

    task 1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,28 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val b = intArrayOf()
+        var i = 1
+//        var c = 1
+        if (!b.contentEquals(sadArray)) {
+            println("success")
+            println(sadArray[0])
+//            b[0] = sadArray[0]
+            while (i <= sadArray.size) {
+                if ((sadArray[i + 1] + sadArray[i - 1]) > sadArray[i]) {
+//                    b[c] = sadArray[i]
+                    print(sadArray[i])
+                    i++
+////                    c++
+                }
+//
+            }
+
+        }
+
+//        b.forEach(::print)
+        return b
+//        throw NotImplementedError("Not implemented")
     }
+
 }
